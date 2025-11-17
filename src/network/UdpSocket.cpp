8333eb715e44bced5831230520ddcf49e10753b8
Paul-Antoine SALMON <paul-antoine.salmon@epitech.eu>
2025-11-17 18:22:06 +0100

    feat(network): implement UdpSocket class with basic methods and structure

--- src/network/UdpSocket.cpp
+++ src/network/UdpSocket.cpp
@@ -1,6 +1,49 @@
-// Placeholder UDP socket implementation
+/*
+** EPITECH PROJECT, 2025
+** Rtype
+** File description:
+** UdpSocket
+*/
+
+#include "rtype/network/UdpSocket.hpp"
+
 namespace rtype::network {
 
-struct UdpSocket {};
+UdpSocket::UdpSocket() : socket_(-1) {}
+
+UdpSocket::~UdpSocket() {
+    close();
+}
+
+bool UdpSocket::bind(uint16_t port) {
+    // Placeholder - would create and bind actual socket
+    (void)port;
+    return false;
+}
+
+bool UdpSocket::connect(const std::string& host, uint16_t port) {
+    // Placeholder
+    (void)host;
+    (void)port;
+    return false;
+}
+
+int UdpSocket::send(const void* data, size_t size) {
+    // Placeholder
+    (void)data;
+    (void)size;
+    return -1;
+}
+
+int UdpSocket::receive(void* buffer, size_t maxSize) {
+    // Placeholder
+    (void)buffer;
+    (void)maxSize;
+    return -1;
+}
+
+void UdpSocket::close() {
+    socket_ = -1;
+}
 
 } // namespace rtype::network
