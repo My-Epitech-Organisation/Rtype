7081da1effd77dec4c2cc40f0e0fc355e3106c95
Paul-Antoine SALMON <paul-antoine.salmon@epitech.eu>
2025-11-17 18:22:03 +0100

    feat(network): implement Serializer class with serialize and deserialize methods

--- src/network/Serializer.cpp
+++ src/network/Serializer.cpp
@@ -1,6 +1,33 @@
-// Placeholder Serializer implementation
+/*
+** EPITECH PROJECT, 2025
+** Rtype
+** File description:
+** Serializer
+*/
+
+#include "rtype/network/Serializer.hpp"
+
 namespace rtype::network {
 
-struct Serializer {};
+std::vector<uint8_t> Serializer::serialize(const Packet& packet) {
+    std::vector<uint8_t> result;
+    result.push_back(static_cast<uint8_t>(packet.type()));
+    const auto& data = packet.data();
+    result.insert(result.end(), data.begin(), data.end());
+    return result;
+}
+
+Packet Serializer::deserialize(const std::vector<uint8_t>& data) {
+    if (data.empty()) {
+        return Packet();
+    }
+
+    Packet packet(static_cast<PacketType>(data[0]));
+    if (data.size() > 1) {
+        std::vector<uint8_t> payload(data.begin() + 1, data.end());
+        packet.setData(payload);
+    }
+    return packet;
+}
 
 } // namespace rtype::network
