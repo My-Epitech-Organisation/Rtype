42a4ef4de0bf8076d264df70f9f28d1b591b7986
Paul-Antoine SALMON <paul-antoine.salmon@epitech.eu>
2025-11-17 18:21:20 +0100

    feat(client): create RenderSystem.cpp for client system initialization

---
+++ src/games/rtype/client/Systems/RenderSystem.cpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2025
+** Rtype
+** File description:
+** RenderSystem
+*/
+
+// Placeholder for client systems
+namespace rtype::games::rtype::client {
+
+void initClientSystems() {
+    // Client system initialization
+}
+
+} // namespace rtype::games::rtype::client
