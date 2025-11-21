e876dc632e81193709d5b478ae4472965e98251f
Paul-Antoine SALMON <paul-antoine.salmon@epitech.eu>
2025-11-21 15:56:57 +0100

    fix(docs): remove unnecessary whitespace in public API headers

--- include/rtype/engine/IRegistry.hpp
+++ include/rtype/engine/IRegistry.hpp
@@ -13,24 +13,23 @@ namespace rtype::engine {
 
 /**
  * @brief Entity handle for the ECS system
- * 
+ *
  * Lightweight handle to reference entities in the registry.
  * Use with IRegistry to create, destroy and manage entities.
  */
 class EntityHandle {
 public:
     using Id = std::size_t;
-    
+
     EntityHandle() : id_(0), valid_(false) {}
     explicit EntityHandle(Id id) : id_(id), valid_(true) {}
-    
-    Id id() const { return id_; }
+       Id id() const { return id_; }
     bool isValid() const { return valid_; }
-    
+
     bool operator==(const EntityHandle& other) const {
         return id_ == other.id_ && valid_ == other.valid_;
     }
-    
+
     bool operator!=(const EntityHandle& other) const {
         return !(*this == other);
     }
@@ -42,11 +41,11 @@ private:
 
 /**
  * @brief Public interface for the Entity Component System registry
- * 
+ *
  * Use this interface to create and manage entities in the game.
  * The registry is the core of the ECS architecture and manages
  * entity lifecycles.
- * 
+ *
  * Example usage:
  * @code
  * IRegistry& registry = getRegistry();
@@ -57,25 +56,25 @@ private:
 class IRegistry {
 public:
     virtual ~IRegistry() = default;
-    
+
     /**
      * @brief Create a new entity
      * @return Handle to the created entity
      */
     virtual EntityHandle createEntity() = 0;
-    
+
     /**
      * @brief Destroy an entity
      * @param entity The entity to destroy
      */
     virtual void destroyEntity(const EntityHandle& entity) = 0;
-    
+
     /**
      * @brief Get the number of active entities
      * @return Number of entities currently alive
      */
     virtual std::size_t entityCount() const = 0;
-    
+
     /**
      * @brief Clear all entities from the registry
      */
