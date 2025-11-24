770fb875ae18a16ba70eaab2e181e20701c1a8ed
Paul-Antoine SALMON <paul-antoine.salmon@epitech.eu>
2025-11-24 15:31:58 +0100

    style(qt_network_poc): clean up whitespace and improve code formatting

--- examples/qt_network_poc/udp_server.cpp
+++ examples/qt_network_poc/udp_server.cpp
@@ -17,15 +17,15 @@ class UdpServer : public QObject {
 public:
     UdpServer(quint16 port, QObject *parent = nullptr) : QObject(parent) {
         socket = new QUdpSocket(this);
-        
+
         if (!socket->bind(QHostAddress::Any, port)) {
             qCritical() << "Failed to bind to port" << port;
             QCoreApplication::exit(1);
             return;
         }
-        
+
         std::cout << "Qt Network UDP Server listening on port " << port << std::endl;
-        
+
         connect(socket, &QUdpSocket::readyRead, this, &UdpServer::processPendingDatagrams);
     }
 
@@ -38,11 +38,11 @@ private slots:
             quint16 senderPort;
 
             socket->readDatagram(datagram.data(), datagram.size(), &sender, &senderPort);
-            
+
             QString message = QString::fromUtf8(datagram);
-            std::cout << "Received: \"" << message.toStdString() << "\" from " 
+            std::cout << "Received: \"" << message.toStdString() << "\" from "
                       << sender.toString().toStdString() << ":" << senderPort << std::endl;
-            
+
             // Echo back
             QString response = "Echo: " + message;
             socket->writeDatagram(response.toUtf8(), sender, senderPort);
@@ -61,14 +61,14 @@ int main(int argc, char *argv[]) {
 
     // QCoreApplication IS required for Qt event loop
     QCoreApplication app(argc, argv);
-    
+
     quint16 port = QString(argv[1]).toUShort();
-    
+
     std::cout << "Qt Network UDP Server PoC" << std::endl;
     std::cout << "Note: QCoreApplication is REQUIRED for event loop" << std::endl;
-    
+
     UdpServer server(port);
-    
+
     return app.exec();
 }
 
