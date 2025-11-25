88090a5be0a576ce448f0c6c2cdf39fc00fd1e91
Nduboi <63715705+nduboi@users.noreply.github.com>
2025-11-25 09:11:27 +0100

    feat: Refactor code structure for improved readability and maintainability

--- src/client/Graphic/Graphic.cpp
+++ src/client/Graphic/Graphic.cpp
@@ -6,7 +6,12 @@
 */
 
 #include "Graphic.hpp"
+#include "SFML/Window/Event.hpp"
+#include "./SFML/Graphics.hpp"
+#include "../assets/r-typesheet42.h"
 #include <optional>
+#include <thread>
+#include <chrono>
 
 void RTypeClient::Graphic::pollEvents() {
     while (const std::optional event = this->_window.pollEvent()) {
@@ -35,7 +40,10 @@ RTypeClient::Graphic::~Graphic() {
 
 RTypeClient::Graphic::Graphic() :
     _window(sf::VideoMode({800, 600}), "R-TYPE window SFML"),
-    _vesselTexture("assets/r-typesheet42.gif"),
+    _vesselTexture(r_typesheet42_gif, r_typesheet42_gif_len),
     _vessel(this->_vesselTexture)
 {
+    this->_vessel.setTextureRect(sf::IntRect({0, 0}, {34, 20}));
+    this->_vessel.setScale({4.0f, 4.0f});
+    this->_vessel.setPosition({400.0f, 300.0f});
 }
