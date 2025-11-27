2ca566d774fe0fa2e01889a8320e154c79e4bf79
Samuel Tesson <samuel.tesson@epitech.eu>
2025-11-27 10:01:27 +0100

    refactor(ecs): reorganize include directives and improve formatting in View

--- src/engine/ecs/view/View.hpp
+++ src/engine/ecs/view/View.hpp
@@ -5,73 +5,75 @@
 ** View
 */
 
-#ifndef ECS_VIEW_VIEW_HPP
-    #define ECS_VIEW_VIEW_HPP
-    #include "../core/Entity.hpp"
-    #include "../storage/SparseSet.hpp"
-    #include <tuple>
-    #include <algorithm>
-    #include <functional>
+#ifndef SRC_ENGINE_ECS_VIEW_VIEW_HPP_
+#define SRC_ENGINE_ECS_VIEW_VIEW_HPP_
+
+#include <algorithm>
+#include <functional>
+#include <tuple>
+
+#include "../core/Entity.hpp"
+#include "../storage/SparseSet.hpp"
 
 namespace ECS {
 
-    class Registry;
+class Registry;
 
-    template<typename, typename>
-    class ExcludeView;
+template<typename, typename>
+class ExcludeView;
+
+/**
+ * @brief Non-owning view for iterating entities with specific components.
+ *
+ * Automatically selects the smallest component set for iteration to minimize work.
+ * Views are lightweight and designed for single-threaded traversal.
+ *
+ * Example:
+ *   auto view = registry.view<Position, Velocity>();
+ *   view.each([](Entity e, Position& p, Velocity& v) {
+ *       p.x += v.dx;
+ *   });
+ */
+template<typename... Components>
+class View {
+ public:
+    explicit View(std::reference_wrapper<Registry> registry);
 
     /**
-     * @brief Non-owning view for iterating entities with specific components.
-     *
-     * Automatically selects the smallest component set for iteration to minimize work.
-     * Views are lightweight and designed for single-threaded traversal.
-     *
-     * Example:
-     *   auto view = registry.view<Position, Velocity>();
-     *   view.each([](Entity e, Position& p, Velocity& v) {
-     *       p.x += v.dx;
-     *   });
+     * @brief Applies function to each entity with all required components.
+     * @param func Callable with signature (Entity, Components&...)
      */
-    template<typename... Components>
-    class View {
-    public:
-        explicit View(std::reference_wrapper<Registry> registry);
+    template<typename Func>
+    void each(Func&& func);
 
-        /**
-         * @brief Applies function to each entity with all required components.
-         * @param func Callable with signature (Entity, Components&...)
-         */
-        template<typename Func>
-        void each(Func&& func);
+    /**
+     * @brief Creates an exclude view that filters out entities with specified components.
+     * @return ExcludeView that excludes entities with Excluded components
+     */
+    template<typename... Excluded>
+    auto exclude();
 
-        /**
-         * @brief Creates an exclude view that filters out entities with specified components.
-         * @return ExcludeView that excludes entities with Excluded components
-         */
-        template<typename... Excluded>
-        auto exclude();
+ private:
+    template<typename T>
+    using PoolPtr = std::reference_wrapper<ISparseSet>;
 
-    private:
-        template<typename T>
-        using PoolPtr = std::reference_wrapper<ISparseSet>;
+    std::reference_wrapper<Registry> registry;
+    std::tuple<PoolPtr<Components>...> pools;
+    size_t _smallestPoolIndex = 0;
 
-        std::reference_wrapper<Registry> registry;
-        std::tuple<PoolPtr<Components>...> pools;
-        size_t _smallestPoolIndex = 0;
+    template<typename Func, size_t... Is>
+    void eachImpl(Func&& func, std::index_sequence<Is...> /*unused*/);
 
-        template<typename Func, size_t... Is>
-        void eachImpl(Func&& func, std::index_sequence<Is...>);
+    template<size_t... Is>
+    auto findSmallestPool(std::index_sequence<Is...> /*unused*/) -> size_t;
 
-        template<size_t... Is>
-        size_t findSmallestPool(std::index_sequence<Is...>);
+    template<typename T>
+    auto getComponentData(Entity entity, const ISparseSet& pool) -> decltype(auto);
 
-        template<typename T>
-        decltype(auto) getComponentData(Entity entity, const ISparseSet& pool);
+    template<typename, typename>
+    friend class ExcludeView;
+};
 
-        template<typename, typename>
-        friend class ExcludeView;
-    };
+}  // namespace ECS
 
-} // namespace ECS
-
-#endif // ECS_VIEW_VIEW_HPP
+#endif  // SRC_ENGINE_ECS_VIEW_VIEW_HPP_
