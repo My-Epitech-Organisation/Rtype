3ad3d2a39d21548940cd17b2d7352e575d5633b7
ctalneau <clement.talneau@epitech.eu>
2025-11-27 14:58:28 +0100

    fix(movement): update movement function signature and return type

--- src/games/rtype/shared/Systems/MovementSystem.cpp
+++ src/games/rtype/shared/Systems/MovementSystem.cpp
@@ -5,15 +5,16 @@
 ** MovementSystem
 */
 
-#include "../Components.hpp"
+#include "MovementSystem.hpp"
 
 namespace rtype::games::rtype::shared {
 
 // Minimal movement system placeholder
-void updateMovement(TransformComponent& transform,
+TransformComponent updateMovement(TransformComponent transform,
                     const VelocityComponent& velocity, float deltaTime) {
     transform.x += velocity.vx * deltaTime;
     transform.y += velocity.vy * deltaTime;
+    return transform;
 }
 
 }  // namespace rtype::games::rtype::shared
---
+++ src/games/rtype/shared/Systems/MovementSystem.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2025
+** Rtype
+** File description:
+** MovementSystem
+*/
+
+#pragma once
+
+#include "../Components.hpp"
+
+namespace rtype::games::rtype::shared {
+
+TransformComponent updateMovement(TransformComponent transform, const VelocityComponent& velocity, float deltaTime);
+
+}  // namespace rtype::games::rtype::shared
