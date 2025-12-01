1118267e9fa50ada6fd68499d6251eb5570a47b6
Samuel Tesson <samuel.tesson@epitech.eu>
2025-12-01 17:21:32 +0100

    fix(argparser): correct include guards and formatting in NumberParser.hpp

--- src/common/ArgParser/NumberParser.hpp
+++ src/common/ArgParser/NumberParser.hpp
@@ -5,16 +5,18 @@
 ** NumberParser - Numeric parsing utilities
 */
 
-#ifndef RTYPE_COMMON_ARGPARSER_NUMBERPARSER_HPP
-    #define RTYPE_COMMON_ARGPARSER_NUMBERPARSER_HPP
+#ifndef SRC_COMMON_ARGPARSER_NUMBERPARSER_HPP_
+#define SRC_COMMON_ARGPARSER_NUMBERPARSER_HPP_
 
-    #include "../Logger/Macros.hpp"
-    #include <format>
-    #include <limits>
-    #include <optional>
-    #include <stdexcept>
-    #include <string>
-    #include <string_view>
+#include <cstdint>
+#include <format>
+#include <limits>
+#include <optional>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+
+#include "../Logger/Macros.hpp"
 
 namespace rtype {
 
@@ -30,18 +32,17 @@ namespace rtype {
 template<typename T>
 [[nodiscard]] std::optional<T> parseNumber(std::string_view str, std::string_view name,
                               T minVal = std::numeric_limits<T>::min(),
-                              T maxVal = std::numeric_limits<T>::max()) noexcept
-{
+                              T maxVal = std::numeric_limits<T>::max()) noexcept {
     try {
         const std::string input(str);
         std::size_t pos = 0;
-        const long long value = std::stoll(input, &pos);
+        const int64_t value = std::stoll(input, &pos);
 
         if (pos != input.size()) {
             LOG_ERROR(std::format("Invalid {}: '{}' is not a valid number", name, str));
             return std::nullopt;
         }
-        if (value < static_cast<long long>(minVal) || value > static_cast<long long>(maxVal)) {
+        if (value < static_cast<int64_t>(minVal) || value > static_cast<int64_t>(maxVal)) {
             LOG_ERROR(std::format("Invalid {}: must be between {} and {}", name, minVal, maxVal));
             return std::nullopt;
         }
@@ -55,6 +56,6 @@ template<typename T>
     }
 }
 
-} // namespace rtype
+}  // namespace rtype
 
-#endif // RTYPE_COMMON_ARGPARSER_NUMBERPARSER_HPP
+#endif  // SRC_COMMON_ARGPARSER_NUMBERPARSER_HPP_
