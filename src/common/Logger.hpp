89685909da34598dc85e059ce5a5cea96e21db95
Samuel Tesson <samuel.tesson@epitech.eu>
2025-12-01 17:41:11 +0100

    feat(logger): add main include file for logging utilities

---
+++ src/common/Logger.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2025
+** Rtype
+** File description:
+** Logger - Main include file for logging utilities
+*/
+
+#ifndef SRC_COMMON_LOGGER_HPP_
+#define SRC_COMMON_LOGGER_HPP_
+
+#include "Logger/LogLevel.hpp"
+#include "Logger/Timestamp.hpp"
+#include "Logger/FileWriter.hpp"
+#include "Logger/Logger.hpp"
+#include "Logger/Macros.hpp"
+
+#endif  // SRC_COMMON_LOGGER_HPP_
