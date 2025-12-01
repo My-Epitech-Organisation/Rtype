bb35d3dacbb4e550426521a801769113b326da8e
Samuel Tesson <samuel.tesson@epitech.eu>
2025-12-01 17:10:12 +0100

    feat(argparser): add main argument parser class with flag and option handling

---
+++ src/common/ArgParser.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2025
+** Rtype
+** File description:
+** ArgParser - Main include file for argument parsing utilities
+*/
+
+#ifndef RTYPE_COMMON_ARGPARSER_HPP
+    #define RTYPE_COMMON_ARGPARSER_HPP
+
+    #include "ArgParser/ParseResult.hpp"
+    #include "ArgParser/Option.hpp"
+    #include "ArgParser/NumberParser.hpp"
+    #include "ArgParser/ArgParser.hpp"
+
+#endif // RTYPE_COMMON_ARGPARSER_HPP
