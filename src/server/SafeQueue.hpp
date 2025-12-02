3b5c3c2e43124477ad0ffb4c8d8f50bcb62680a1
ctalneau <clement.talneau@epitech.eu>
2025-12-02 09:35:54 +0100

    style(SafeQueue): format code for consistency and readability

--- src/server/SafeQueue.hpp
+++ src/server/SafeQueue.hpp
@@ -7,34 +7,34 @@
 
 #pragma once
 
-#include <queue>
 #include <mutex>
 #include <optional>
+#include <queue>
 
-template<typename T>
+template <typename T>
 class SafeQueue {
-    public:
-        void push(const T& item) {
-            std::lock_guard<std::mutex> lock(mutex_);
-            queue_.push(item);
-        }
+   public:
+    void push(const T& item) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        queue_.push(item);
+    }
 
-        std::optional<T> pop() {
-            std::lock_guard<std::mutex> lock(mutex_);
-            if (queue_.empty()) {
-                return std::nullopt;
-            }
-            T item = queue_.front();
-            queue_.pop();
-            return item;
+    std::optional<T> pop() {
+        std::lock_guard<std::mutex> lock(mutex_);
+        if (queue_.empty()) {
+            return std::nullopt;
         }
+        T item = queue_.front();
+        queue_.pop();
+        return item;
+    }
 
-        size_t size() {
-            std::lock_guard<std::mutex> lock(mutex_);
-            return queue_.size();
-        }
+    size_t size() {
+        std::lock_guard<std::mutex> lock(mutex_);
+        return queue_.size();
+    }
 
-    private:
-        std::queue<T> queue_;
-        std::mutex mutex_;
+   private:
+    std::queue<T> queue_;
+    std::mutex mutex_;
 };
