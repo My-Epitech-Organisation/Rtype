d6a630c008fd66d858c90488589c4ea4259c2a09
ctalneau <clement.talneau@epitech.eu>
2025-12-02 11:50:24 +0100

    feat(SafeQueue): implement thread-safe queue with push/pop methods and add tests

--- tests/common/SafeQueue_test.cpp
+++ tests/common/SafeQueue_test.cpp
@@ -2,7 +2,7 @@
 #include <thread>
 #include <vector>
 #include <algorithm>
-#include "../../src/server/SafeQueue.hpp"
+#include "../../src/common/SafeQueue/SafeQueue.hpp"
 
 TEST(SafeQueueTest, PushAndPop) {
     SafeQueue<int> queue;
