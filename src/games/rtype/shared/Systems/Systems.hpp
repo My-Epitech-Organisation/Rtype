3375a697823293b6c3a196bcfe11c3b7fb01d1c0
Samuel Tesson <samuel.tesson@epitech.eu>
2025-12-03 13:39:09 +0100

    feat(Systems): add aggregate header for all shared systems

---
+++ src/games/rtype/shared/Systems/Systems.hpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2025
+** Rtype
+** File description:
+** Systems - Aggregate header for all shared systems
+*/
+
+#pragma once
+
+#include "AISystem/AISystem.hpp"
+#include "../../engine/ISystem.hpp"
+#include "Movements/MovementSystem.hpp"
