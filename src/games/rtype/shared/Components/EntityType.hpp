ae3e9e15d2437f29aea42eaa54ac3ba874bc3821
Samuel Tesson <samuel.tesson@epitech.eu>
2025-12-03 13:56:21 +0100

    style(EntityType): update comment formatting to use single-line style

--- src/games/rtype/shared/Components/EntityType.hpp
+++ src/games/rtype/shared/Components/EntityType.hpp
@@ -18,12 +18,12 @@ namespace rtype::games::rtype::shared {
  * Used to identify entity types when synchronizing over network.
  */
 enum class EntityType : uint8_t {
-    Unknown = 0,  ///< Unknown/uninitialized entity
-    Player,       ///< Player-controlled ship
-    Enemy,        ///< Enemy entity
-    Projectile,   ///< Projectile (bullet, missile, etc.)
-    Pickup,       ///< Collectible item (power-up, etc.)
-    Obstacle      ///< Static or moving obstacle
+    Unknown = 0,  // Unknown/uninitialized entity
+    Player,       // Player-controlled ship
+    Enemy,        // Enemy entity
+    Projectile,   // Projectile (bullet, missile, etc.)
+    Pickup,       // Collectible item (power-up, etc.)
+    Obstacle      // Static or moving obstacle
 };
 
 }  // namespace rtype::games::rtype::shared
