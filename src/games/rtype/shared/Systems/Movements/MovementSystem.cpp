5bc4842faf22d7000a01a0e9b8a522577e3ef6b7
Samuel Tesson <samuel.tesson@epitech.eu>
2025-12-03 13:56:55 +0100

    refactor(MovementSystem): inline movement update logic for clarity

--- src/games/rtype/shared/Systems/Movements/MovementSystem.cpp
+++ src/games/rtype/shared/Systems/Movements/MovementSystem.cpp
@@ -14,14 +14,9 @@ void MovementSystem::update(ECS::Registry& registry, float deltaTime) {
 
     view.each([deltaTime](ECS::Entity /*entity*/, TransformComponent& transform,
                           const VelocityComponent& velocity) {
-        updateMovement(transform, velocity, deltaTime);
+        transform.x += velocity.vx * deltaTime;
+        transform.y += velocity.vy * deltaTime;
     });
-}
-
-void updateMovement(TransformComponent& transform,
-                    const VelocityComponent& velocity, float deltaTime) {
-    transform.x += velocity.vx * deltaTime;
-    transform.y += velocity.vy * deltaTime;
 }
 
 }  // namespace rtype::games::rtype::shared
