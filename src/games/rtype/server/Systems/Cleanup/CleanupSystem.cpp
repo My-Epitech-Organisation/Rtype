1cb473eaf2602ae9d4036b313cbed37a84c825ae
Samuel Tesson <samuel.tesson@epitech.eu>
2025-12-03 13:37:42 +0100

    feat(CleanupSystem): implement CleanupSystem to manage out-of-bounds entity destruction

---
+++ src/games/rtype/server/Systems/Cleanup/CleanupSystem.cpp
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2025
+** Rtype
+** File description:
+** CleanupSystem - Marks out-of-bounds entities for destruction
+*/
+
+#include "CleanupSystem.hpp"
+
+#include "../../../shared/Components.hpp"
+
+namespace rtype::games::rtype::server {
+
+using shared::DestroyTag;
+using shared::EnemyTag;
+using shared::TransformComponent;
+
+CleanupSystem::CleanupSystem(EventEmitter emitter, CleanupConfig config)
+    : _emitEvent(std::move(emitter)), _config(config) {}
+
+void CleanupSystem::update(ECS::Registry& registry, float /*deltaTime*/) {
+    auto view = registry.view<TransformComponent, EnemyTag>();
+
+    view.each([this, &registry](ECS::Entity entity,
+                                const TransformComponent& transform,
+                                const EnemyTag& /*tag*/) {
+        bool outOfBounds = transform.x < _config.leftBoundary ||
+                           transform.x > _config.rightBoundary ||
+                           transform.y < _config.topBoundary ||
+                           transform.y > _config.bottomBoundary;
+
+        if (outOfBounds && !registry.hasComponent<DestroyTag>(entity)) {
+            registry.emplaceComponent<DestroyTag>(entity, DestroyTag{});
+        }
+    });
+}
+
+}  // namespace rtype::games::rtype::server
