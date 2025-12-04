20fb408b428b99f386943a7dc1b18764415f49fa
Nduboi <63715705+nduboi@users.noreply.github.com>
2025-12-05 00:16:36 +0100

    styles(cpplint): Fix cpplint errors

--- src/games/rtype/client/Systems/MovementSystem.cpp
+++ src/games/rtype/client/Systems/MovementSystem.cpp
@@ -13,8 +13,10 @@
 
 void MovementSystem::update(const std::shared_ptr<ECS::Registry>& registry,
                             float dt) {
-    registry->view<rtype::games::rtype::shared::VelocityComponent, Position, Image>().each(
-        [dt](auto _, auto& velocity, auto& position, auto& spriteData) {
+    registry
+        ->view<rtype::games::rtype::shared::VelocityComponent, Position,
+               Image>()
+        .each([dt](auto _, auto& velocity, auto& position, auto& spriteData) {
             position.x += velocity.vx * dt;
             position.y += velocity.vy * dt;
 
