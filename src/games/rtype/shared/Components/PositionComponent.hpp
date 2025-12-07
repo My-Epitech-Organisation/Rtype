ae51221b20ded59bb9f7bf9df994f4a4c8087913
Samuel Tesson <samuel.tesson@epitech.eu>
2025-12-07 12:18:10 +0000

    refactor(PositionComponent): enhance readability by initializing position coordinates and improving documentation

--- src/games/rtype/shared/Components/PositionComponent.hpp
+++ src/games/rtype/shared/Components/PositionComponent.hpp
@@ -7,10 +7,25 @@
 
 #ifndef SRC_GAMES_RTYPE_SHARED_COMPONENTS_POSITIONCOMPONENT_HPP_
 #define SRC_GAMES_RTYPE_SHARED_COMPONENTS_POSITIONCOMPONENT_HPP_
+
 namespace rtype::games::rtype::shared {
+
+/**
+ * @brief Component storing entity position in 2D world space.
+ *
+ * Used for all entities that have a position in the game world.
+ * Combined with VelocityComponent by MovementSystem to update positions.
+ *
+ * Coordinate system:
+ * - Origin (0,0) is at top-left of the world
+ * - X increases to the right
+ * - Y increases downward
+ */
 struct Position {
-    float x;
-    float y;
+    float x = 0.0f;  ///< X coordinate in pixels
+    float y = 0.0f;  ///< Y coordinate in pixels
 };
+
 }  // namespace rtype::games::rtype::shared
+
 #endif  // SRC_GAMES_RTYPE_SHARED_COMPONENTS_POSITIONCOMPONENT_HPP_
