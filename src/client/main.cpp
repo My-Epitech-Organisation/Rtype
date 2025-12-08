f054e3334645b29b72a786cd9429abd84154648b
Samuel Tesson <samuel.tesson@epitech.eu>
2025-12-08 12:41:52 +0000

    feat(client): Update main entry point to use ClientApp configuration

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,3 +1,9 @@
+/*
+** EPITECH PROJECT, 2025
+** r-type
+** File description:
+** Client main entry point
+*/
 
 #include <exception>
 #include <iostream>
@@ -7,12 +13,18 @@
 auto main(int argc, char** argv) -> int {
     (void)argc;
     (void)argv;
-    auto registry = std::make_shared<ECS::Registry>();
+
     try {
-        ClientApp client(registry);
+        ClientApp::Config config;
+        // TODO: Parse command line arguments for server host/port
+        // config.defaultServerHost = "127.0.0.1";
+        // config.defaultServerPort = 4242;
+
+        ClientApp client(config);
         client.run();
     } catch (const std::exception& e) {
         std::cerr << "Program exited with an error: " << e.what() << std::endl;
+        return 1;
     }
     return 0;
 }
