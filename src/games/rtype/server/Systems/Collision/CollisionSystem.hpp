83daa4a51a554db91f2aa0b612d719daa94ceb4a
Paul-Antoine SALMON <paul-antoine.salmon@epitech.eu>
2025-12-08 13:35:46 +0100

    feat(server): implement AABB collision handling in CollisionSystem

---
+++ src/games/rtype/server/Systems/Collision/CollisionSystem.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2025
+** Rtype
+** File description:
+** CollisionSystem - Server-side AABB collision handling
+*/
+
+#pragma once
+
+#include <rtype/engine.hpp>
+
+namespace rtype::games::rtype::server {
+
+class CollisionSystem : public ::rtype::engine::ASystem {
+   public:
+    CollisionSystem();
+    void update(ECS::Registry& registry, float deltaTime) override;
+};
+
+}  // namespace rtype::games::rtype::server
