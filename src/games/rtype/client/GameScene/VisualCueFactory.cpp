e45eb4c5ac86135a0ee79e5eb25bdf022bb72d50
Samuel Tesson <samuel.tesson@epitech.eu>
2025-12-12 21:25:08 +0000

    feat(visual): add createDamagePopup method for displaying damage numbers

--- src/games/rtype/client/GameScene/VisualCueFactory.cpp
+++ src/games/rtype/client/GameScene/VisualCueFactory.cpp
@@ -9,10 +9,14 @@
 #include <utility>
 
 #include "AllComponents.hpp"
+#include "Logger/Macros.hpp"
 #include "Components/RectangleComponent.hpp"
+#include "Components/TagComponent.hpp"
+#include "Components/TextComponent.hpp"
 #include "Components/ZIndexComponent.hpp"
 #include "Graphic/Accessibility.hpp"
 #include "games/rtype/shared/Components/LifetimeComponent.hpp"
+#include "games/rtype/shared/Components/VelocityComponent.hpp"
 
 namespace rtype::games::rtype::client {
 
@@ -43,6 +47,34 @@ void VisualCueFactory::createFlash(ECS::Registry& registry,
     registry.emplaceComponent<ZIndex>(entity, zIndex);
     registry.emplaceComponent<::rtype::games::rtype::shared::LifetimeComponent>(
         entity, lifetime);
+}
+
+void VisualCueFactory::createDamagePopup(ECS::Registry& registry,
+                                         const sf::Vector2f& position,
+                                         int damage,
+                                         const sf::Font& font,
+                                         const sf::Color& color) {
+    auto entity = registry.spawnEntity();
+    LOG_DEBUG("[VisualCueFactory] Damage popup entity=" +
+              std::to_string(entity.id) + " dmg=" +
+              std::to_string(damage));
+
+    std::string damageText = "-" + std::to_string(damage);
+    registry.emplaceComponent<Text>(entity, font, color, 32, damageText);
+
+    registry.emplaceComponent<StaticTextTag>(entity);
+
+    float offsetX = static_cast<float>((rand() % 40) - 20);
+    registry.emplaceComponent<::rtype::games::rtype::shared::Position>(
+        entity, position.x + offsetX, position.y - 20.f);
+
+    registry.emplaceComponent<::rtype::games::rtype::shared::VelocityComponent>(
+        entity, 0.f, -80.f);
+
+    registry.emplaceComponent<ZIndex>(entity, 200);
+
+    registry.emplaceComponent<::rtype::games::rtype::shared::LifetimeComponent>(
+        entity, 1.2f);
 }
 
 }  // namespace rtype::games::rtype::client
--- src/games/rtype/client/GameScene/VisualCueFactory.hpp
+++ src/games/rtype/client/GameScene/VisualCueFactory.hpp
@@ -9,8 +9,10 @@
 #define SRC_GAMES_RTYPE_CLIENT_GAMESCENE_VISUALCUEFACTORY_HPP_
 
 #include <memory>
+#include <string>
 
 #include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/Font.hpp>
 #include <SFML/System/Vector2.hpp>
 
 #include "ECS.hpp"
@@ -26,6 +28,20 @@ class VisualCueFactory {
     static void createFlash(ECS::Registry& registry, const sf::Vector2f& center,
                             const sf::Color& color, float size = 64.f,
                             float lifetime = 0.35f, int zIndex = 50);
+
+    /**
+     * @brief Create a floating damage number popup (WoW style)
+     * @param registry The ECS registry
+     * @param position Position where the number appears
+     * @param damage The damage amount to display
+     * @param font The font to use
+     * @param color Text color (default red for damage)
+     */
+    static void createDamagePopup(ECS::Registry& registry,
+                                  const sf::Vector2f& position,
+                                  int damage,
+                                  const sf::Font& font,
+                                  const sf::Color& color = sf::Color::Red);
 };
 
 }  // namespace rtype::games::rtype::client
