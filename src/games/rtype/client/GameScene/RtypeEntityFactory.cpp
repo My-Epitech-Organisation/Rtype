14bec6dd59a5337a6124813f7027377e215efc5f
Samuel Tesson <samuel.tesson@epitech.eu>
2025-12-12 21:22:58 +0000

    feat(entity): add enemy shooting behavior and implement projectile spawning for pickups and obstacles

--- src/games/rtype/client/GameScene/RtypeEntityFactory.cpp
+++ src/games/rtype/client/GameScene/RtypeEntityFactory.cpp
@@ -23,6 +23,8 @@
 #include "VisualCueFactory.hpp"
 #include "protocol/Payloads.hpp"
 #include "../Systems/PlayerAnimationSystem.hpp"
+#include "../Components/RectangleComponent.hpp"
+#include "../shared/Components/BoundingBoxComponent.hpp"
 
 namespace rtype::games::rtype::client {
 
@@ -58,6 +60,14 @@ RtypeEntityFactory::createNetworkEntityFactory(
             case ::rtype::network::EntityType::Missile:
                 setupMissileEntity(reg, assetsManager, entity);
                 break;
+
+            case ::rtype::network::EntityType::Pickup:
+                setupPickupEntity(reg, entity, event.entityId);
+                break;
+
+            case ::rtype::network::EntityType::Obstacle:
+                setupObstacleEntity(reg, entity, event.entityId);
+                break;
         }
 
         return entity;
@@ -84,6 +94,7 @@ void RtypeEntityFactory::setupPlayerEntity(
     reg.emplaceComponent<TextureRect>(entity, std::pair<int, int>({left, top}),
                                       std::pair<int, int>({width, height}));
     reg.emplaceComponent<Size>(entity, 4, 4);
+    reg.emplaceComponent<::rtype::games::rtype::shared::BoundingBoxComponent>(entity, 128.0f, 64.0f);
     reg.emplaceComponent<shared::HealthComponent>(entity, 1, 1);
     reg.emplaceComponent<PlayerTag>(entity);
     reg.emplaceComponent<BoxingComponent>(entity,
@@ -109,6 +120,7 @@ void RtypeEntityFactory::setupBydosEntity(
     reg.emplaceComponent<TextureRect>(entity, std::pair<int, int>({0, 0}),
                                       std::pair<int, int>({33, 34}));
     reg.emplaceComponent<Size>(entity, 2, 2);
+    reg.emplaceComponent<::rtype::games::rtype::shared::BoundingBoxComponent>(entity, 32.0f, 32.0f);
     reg.emplaceComponent<BoxingComponent>(entity,
                                           sf::FloatRect({0, 0}, {33.f, 34.f}));
     reg.getComponent<BoxingComponent>(entity).outlineColor =
@@ -133,7 +145,8 @@ void RtypeEntityFactory::setupMissileEntity(
         entity, assetsManager->textureManager->get("projectile_player_laser"));
     reg.emplaceComponent<TextureRect>(entity, std::pair<int, int>({0, 0}),
                                       std::pair<int, int>({33, 34}));
-    reg.emplaceComponent<Size>(entity, 2, 2);
+    reg.emplaceComponent<Size>(entity, 1, 1);
+    reg.emplaceComponent<::rtype::games::rtype::shared::BoundingBoxComponent>(entity, 8.0f, 4.0f);
     reg.emplaceComponent<shared::ProjectileTag>(entity);
     reg.emplaceComponent<BoxingComponent>(entity,
                                           sf::FloatRect({0, 0}, {33.f, 34.f}));
@@ -144,7 +157,6 @@ void RtypeEntityFactory::setupMissileEntity(
     reg.emplaceComponent<ZIndex>(entity, 1);
     reg.emplaceComponent<shared::LifetimeComponent>(
         entity, GraphicsConfig::LIFETIME_PROJECTILE);
-    reg.emplaceComponent<Size>(entity, 1, 1);
     reg.emplaceComponent<GameTag>(entity);
     auto lib = reg.getSingleton<std::shared_ptr<AudioLib>>();
     lib->playSFX(*assetsManager->soundManager->get("laser_sfx"));
@@ -154,6 +166,50 @@ void RtypeEntityFactory::setupMissileEntity(
         VisualCueFactory::createFlash(reg, {pos.x, pos.y},
                                       sf::Color(0, 255, 220), 52.f, 0.25f, 10);
     }
+}
+
+void RtypeEntityFactory::setupPickupEntity(ECS::Registry& reg,
+                                           ECS::Entity entity,
+                                           std::uint32_t networkId) {
+    LOG_DEBUG("[RtypeEntityFactory] Adding Pickup components");
+    static const std::array<sf::Color, 4> kColors = {
+        sf::Color(120, 200, 255), sf::Color(170, 120, 255),
+        sf::Color(120, 255, 170), sf::Color(255, 200, 120)};
+    const sf::Color color = kColors[networkId % kColors.size()];
+
+    reg.emplaceComponent<Rectangle>(entity, std::pair<float, float>{24.f, 24.f},
+                                    color, color);
+    reg.getComponent<Rectangle>(entity).outlineThickness = 2.f;
+    reg.getComponent<Rectangle>(entity).outlineColor = sf::Color::White;
+    reg.emplaceComponent<BoxingComponent>(entity,
+                                          sf::FloatRect({0, 0}, {24.f, 24.f}));
+    reg.getComponent<BoxingComponent>(entity).outlineColor = color;
+    reg.getComponent<BoxingComponent>(entity).fillColor = sf::Color(color.r, color.g, color.b, 45);
+    reg.emplaceComponent<ZIndex>(entity, 0);
+    reg.emplaceComponent<GameTag>(entity);
+    reg.emplaceComponent<::rtype::games::rtype::shared::BoundingBoxComponent>(entity, 24.0f, 24.0f);
+}
+
+void RtypeEntityFactory::setupObstacleEntity(ECS::Registry& reg,
+                                             ECS::Entity entity,
+                                             std::uint32_t /*networkId*/) {
+    LOG_DEBUG("[RtypeEntityFactory] Adding Obstacle components");
+    const sf::Color main = sf::Color(110, 110, 120);
+    const sf::Color outline = sf::Color(200, 200, 210);
+
+    reg.emplaceComponent<Rectangle>(entity, std::pair<float, float>{64.f, 64.f},
+                                    main, main);
+    reg.getComponent<Rectangle>(entity).outlineThickness = 2.f;
+    reg.getComponent<Rectangle>(entity).outlineColor = outline;
+    reg.emplaceComponent<BoxingComponent>(entity,
+                                          sf::FloatRect({0, 0}, {64.f, 64.f}));
+    reg.getComponent<BoxingComponent>(entity).outlineColor = outline;
+    reg.getComponent<BoxingComponent>(entity).fillColor =
+        sf::Color(outline.r, outline.g, outline.b, 35);
+    reg.emplaceComponent<ZIndex>(entity, 0);
+    reg.emplaceComponent<GameTag>(entity);
+
+    reg.emplaceComponent<::rtype::games::rtype::shared::BoundingBoxComponent>(entity, 64.0f, 64.0f);
 }
 
 }  // namespace rtype::games::rtype::client
