0b9b70a4c94501ce70fbd52dc16b241db7581e09
Samuel Tesson <samuel.tesson@epitech.eu>
2025-12-12 13:18:44 +0000

    feat(zigzag-behavior): implement ZigZagBehavior for AI movement with alternating vertical steps

---
+++ src/games/rtype/shared/Systems/AISystem/Behaviors/ZigZag/ZigZagBehavior.hpp
@@ -0,0 +1,39 @@
+/*
+** EPITECH PROJECT, 2025
+** Rtype
+** File description:
+** ZigZagBehavior - Alternating vertical steps while moving left
+*/
+
+#pragma once
+
+#include "../IAIBehavior.hpp"
+
+namespace rtype::games::rtype::shared {
+
+/**
+ * @class ZigZagBehavior
+ * @brief AI that moves left while alternating vertical direction at intervals
+ */
+class ZigZagBehavior final : public IAIBehavior {
+   public:
+    explicit ZigZagBehavior(float switchInterval = 0.8F, float stepSpeed = 80.0F)
+        : _switchInterval(switchInterval), _stepSpeed(stepSpeed) {}
+
+    void apply(AIComponent& ai, const TransformComponent& transform,
+               VelocityComponent& velocity, float deltaTime) override;
+
+    [[nodiscard]] AIBehavior getType() const noexcept override {
+        return AIBehavior::ZigZag;
+    }
+
+    [[nodiscard]] const std::string getName() const noexcept override {
+        return "ZigZagBehavior";
+    }
+
+   private:
+    float _switchInterval;
+    float _stepSpeed;
+};
+
+}  // namespace rtype::games::rtype::shared
