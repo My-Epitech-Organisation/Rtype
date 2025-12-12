e3e5566d334f60d038b2e918e08ac90cb221c60f
Samuel Tesson <samuel.tesson@epitech.eu>
2025-12-12 21:26:39 +0000

    feat(destroy): update entity type handling for destroyed entities

--- src/games/rtype/server/Systems/Destroy/DestroySystem.cpp
+++ src/games/rtype/server/Systems/Destroy/DestroySystem.cpp
@@ -12,6 +12,7 @@
 #include <rtype/common.hpp>
 
 #include "../../../shared/Components.hpp"
+#include "protocol/Payloads.hpp"
 
 namespace rtype::games::rtype::server {
 
@@ -64,13 +65,13 @@ void DestroySystem::update(ECS::Registry& registry, float /*deltaTime*/) {
             event.type = engine::GameEventType::EntityDestroyed;
             event.entityNetworkId = netIdComp.networkId;
             if (isEnemy) {
-                event.entityType = static_cast<uint8_t>(EntityType::Enemy);
+                event.entityType = static_cast<uint8_t>(::rtype::network::EntityType::Bydos);
             } else if (isProjectile) {
-                event.entityType = static_cast<uint8_t>(EntityType::Projectile);
+                event.entityType = static_cast<uint8_t>(::rtype::network::EntityType::Missile);
             } else if (isObstacle) {
-                event.entityType = static_cast<uint8_t>(EntityType::Obstacle);
+                event.entityType = static_cast<uint8_t>(::rtype::network::EntityType::Obstacle);
             } else {
-                event.entityType = static_cast<uint8_t>(EntityType::Player);
+                event.entityType = static_cast<uint8_t>(::rtype::network::EntityType::Player);
             }
             _emitEvent(event);
         } else {
