265ecdc2427bb22f804f1e38675030ca499a1642
Samuel Tesson <samuel.tesson@epitech.eu>
2025-12-15 08:34:33 +0000

    feat(ClientDestroySystem): implement entity destruction logic and update header

---
+++ src/client/Graphic/Systems/ClientDestroySystem.cpp
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2025
+** Rtype
+** File description:
+** ClientDestroySystem
+*/
+
+#include "ClientDestroySystem.hpp"
+
+#include <vector>
+
+namespace rtype::games::rtype::client {
+
+void ClientDestroySystem::update(ECS::Registry& registry, float /*deltaTime*/) {
+    auto destroyView = registry.view<shared::DestroyTag>();
+
+    std::vector<ECS::Entity> toDestroy;
+    destroyView.each([&toDestroy](auto entity, const shared::DestroyTag&) {
+        toDestroy.push_back(entity);
+    });
+    auto lifetimeView = registry.view<shared::LifetimeComponent>();
+    lifetimeView.each(
+        [&toDestroy](auto entity, const shared::LifetimeComponent& life) {
+            if (life.remainingTime <= 0.0F) {
+                toDestroy.push_back(entity);
+            }
+        });
+
+    if (!toDestroy.empty()) {
+        LOG_DEBUG("[ClientDestroySystem] Destroying " +
+                    std::to_string(toDestroy.size()) + " entities");
+        for (auto entity : toDestroy) {
+            registry.killEntity(entity);
+        }
+    }
+}
+
+}  // namespace rtype::games::rtype::client
--- src/client/Graphic/Systems/ClientDestroySystem.hpp
+++ src/client/Graphic/Systems/ClientDestroySystem.hpp
@@ -29,29 +29,7 @@ class ClientDestroySystem : public ::rtype::engine::ASystem {
    public:
     ClientDestroySystem() : ASystem("ClientDestroySystem") {}
 
-    void update(ECS::Registry& registry, float /*deltaTime*/) override {
-        auto destroyView = registry.view<shared::DestroyTag>();
-
-        std::vector<ECS::Entity> toDestroy;
-        destroyView.each([&toDestroy](auto entity, const shared::DestroyTag&) {
-            toDestroy.push_back(entity);
-        });
-        auto lifetimeView = registry.view<shared::LifetimeComponent>();
-        lifetimeView.each(
-            [&toDestroy](auto entity, const shared::LifetimeComponent& life) {
-                if (life.remainingTime <= 0.0F) {
-                    toDestroy.push_back(entity);
-                }
-            });
-
-        if (!toDestroy.empty()) {
-            LOG_DEBUG("[ClientDestroySystem] Destroying " +
-                      std::to_string(toDestroy.size()) + " entities");
-            for (auto entity : toDestroy) {
-                registry.killEntity(entity);
-            }
-        }
-    }
+    void update(ECS::Registry& registry, float /*deltaTime*/) override;
 };
 
 }  // namespace rtype::games::rtype::client
