1d9cd042b87529e8c76875cdc2d1af4c1b67b33c
ctalneau <clement.talneau@epitech.eu>
2025-12-17 17:20:57 +0100

    fix(cmake): enhance error handling for CPM.cmake download failure

--- cmake/CPM.cmake
+++ cmake/CPM.cmake
@@ -10,7 +10,21 @@ if(NOT EXISTS "${CPM_DOWNLOAD_LOCATION}")
         "https://github.com/cpm-cmake/CPM.cmake/releases/download/v${CPM_DOWNLOAD_VERSION}/CPM.cmake"
         "${CPM_DOWNLOAD_LOCATION}"
         TLS_VERIFY ON
+        STATUS CPM_DOWNLOAD_STATUS
     )
+
+    list(GET CPM_DOWNLOAD_STATUS 0 CPM_DOWNLOAD_STATUS_CODE)
+    list(GET CPM_DOWNLOAD_STATUS 1 CPM_DOWNLOAD_ERROR_MESSAGE)
+
+    if(NOT CPM_DOWNLOAD_STATUS_CODE EQUAL 0)
+        file(REMOVE "${CPM_DOWNLOAD_LOCATION}")
+        message(FATAL_ERROR
+            "[CPM] Failed to download CPM.cmake v${CPM_DOWNLOAD_VERSION}\n"
+            "Error: ${CPM_DOWNLOAD_ERROR_MESSAGE}\n"
+            "URL: https://github.com/cpm-cmake/CPM.cmake/releases/download/v${CPM_DOWNLOAD_VERSION}/CPM.cmake\n"
+            "Please check your network connection or download the file manually."
+        )
+    endif()
 endif()
 
 include("${CPM_DOWNLOAD_LOCATION}")
