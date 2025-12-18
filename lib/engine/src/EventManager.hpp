8474257286a2c5177de47a4cb4eb10a8df78b795
Samuel Tesson <samuel.tesson@epitech.eu>
2025-12-18 16:28:27 +0000

    feat(EventManager): add main include file for Event Manager system

---
+++ lib/engine/src/EventManager.hpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2025
+** Rtype
+** File description:
+** EventManager - Main include file for the Event Manager system
+*/
+
+#pragma once
+
+#include "EventManager/CommonEvents.hpp"
+#include "EventManager/EventManager.hpp"
+#include "EventManager/IEvent.hpp"
