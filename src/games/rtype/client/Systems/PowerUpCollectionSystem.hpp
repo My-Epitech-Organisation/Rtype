95a3d5f6d335900357235d02f1e258b802e5fdd9
Samuel Tesson <samuel.tesson@epitech.eu>
2026-01-05 15:32:01 +0000

    refactor: implement PowerUpCollectionSystem for detecting power-up collections and displaying notifications

---
+++ src/games/rtype/client/Systems/PowerUpCollectionSystem.hpp
@@ -0,0 +1,47 @@
+/*
+** EPITECH PROJECT, 2025
+** r-type
+** File description:
+** PowerUpCollectionSystem - Detects power-up collection and shows popups
+*/
+
+#ifndef SRC_GAMES_RTYPE_CLIENT_SYSTEMS_POWERUPCOLLECTIONSYSTEM_HPP_
+#define SRC_GAMES_RTYPE_CLIENT_SYSTEMS_POWERUPCOLLECTIONSYSTEM_HPP_
+
+#include <memory>
+#include <unordered_map>
+
+#include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/Font.hpp>
+
+#include "rtype/engine.hpp"
+#include "../../shared/Components/PowerUpComponent.hpp"
+
+namespace rtype::games::rtype::client {
+
+/**
+ * @brief System that detects when players collect power-ups and displays popup notifications
+ */
+class PowerUpCollectionSystem : public ::rtype::engine::ASystem {
+   public:
+    explicit PowerUpCollectionSystem(std::shared_ptr<sf::Font> font);
+    ~PowerUpCollectionSystem() override = default;
+
+    void update(ECS::Registry& registry, float dt) override;
+
+   private:
+    std::shared_ptr<sf::Font> _font;
+
+    struct PowerUpState {
+        ::rtype::games::rtype::shared::PowerUpType type;
+        float remainingTime;
+    };
+    std::unordered_map<uint32_t, PowerUpState> _lastPowerUpState;
+
+    std::string getPowerUpDisplayName(::rtype::games::rtype::shared::PowerUpType type) const;
+    sf::Color getPowerUpColor(::rtype::games::rtype::shared::PowerUpType type) const;
+};
+
+}  // namespace rtype::games::rtype::client
+
+#endif  // SRC_GAMES_RTYPE_CLIENT_SYSTEMS_POWERUPCOLLECTIONSYSTEM_HPP_
