9f8072fb82403f01966515e0ada1ff9c8d11306e
Nduboi <63715705+nduboi@users.noreply.github.com>
2026-01-06 17:19:45 +0100

    refactor(entrypoint): remove unused includes for cleaner code

--- lib/display/SFML/entrypoint.cpp
+++ lib/display/SFML/entrypoint.cpp
@@ -6,10 +6,7 @@
 */
 
 #include <iostream>
-#include <unistd.h>
 #include "SFMLDisplay.hpp"
-#include "SFMLMusic.hpp"
-#include "SFMLTexture.hpp"
 
 extern "C" {
     rtype::display::IDisplay* createInstanceDisplay(void) {
