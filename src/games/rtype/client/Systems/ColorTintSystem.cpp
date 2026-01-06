a7be49d117e6f743294dc1712b3110f4372f52c5
Samuel Tesson <samuel.tesson@epitech.eu>
2026-01-06 10:01:36 +0000

    refactor: remove unused entity parameter in ColorTintSystem update method

--- src/games/rtype/client/Systems/ColorTintSystem.cpp
+++ src/games/rtype/client/Systems/ColorTintSystem.cpp
@@ -17,7 +17,7 @@ ColorTintSystem::ColorTintSystem()
     : ::rtype::engine::ASystem("ColorTintSystem") {}
 
 void ColorTintSystem::update(ECS::Registry& registry, float dt) {
-    registry.view<Image, ColorTint>().each([](auto entity, auto& spriteData,
+    registry.view<Image, ColorTint>().each([](auto /*entity*/, auto& spriteData,
                                               auto& tint) {
         spriteData.sprite.setColor(sf::Color(tint.r, tint.g, tint.b, tint.a));
     });
