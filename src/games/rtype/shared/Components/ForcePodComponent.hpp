827ba0e2b3016edfefa4cfc516e99ba5a9fb140b
Samuel Tesson <samuel.tesson@epitech.eu>
2026-01-07 14:40:34 +0000

    feat(ForcePodComponent): add ForcePodComponent structure and state enumeration

---
+++ src/games/rtype/shared/Components/ForcePodComponent.hpp
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2025
+** Rtype
+** File description:
+** ForcePodComponent - Force Pod state and configuration data
+*/
+
+#pragma once
+
+#include <cstdint>
+
+namespace rtype::games::rtype::shared {
+
+enum class ForcePodState : uint8_t {
+    Attached = 0,
+    Detached,
+    Returning
+};
+
+struct ForcePodComponent {
+    ForcePodState state{ForcePodState::Attached};
+    float offsetX{0.0F};
+    float offsetY{0.0F};
+    uint32_t ownerNetworkId{0};
+};
+
+}  // namespace rtype::games::rtype::shared
