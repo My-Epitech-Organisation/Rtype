0df04ec47c5ca2413835bc7a893967c7ef26f1b9
Samuel Tesson <samuel.tesson@epitech.eu>
2026-01-09 09:33:29 +0000

    feat(ForcePodShootingSystem): implement automatic shooting logic for Force Pods

---
+++ src/games/rtype/server/Systems/ForcePod/ForcePodShootingSystem.cpp
@@ -0,0 +1,61 @@
+/*
+** EPITECH PROJECT, 2025
+** Rtype
+** File description:
+** ForcePodShootingSystem - Implementation
+*/
+
+#include "ForcePodShootingSystem.hpp"
+
+#include "../Projectile/ProjectileSpawnerSystem.hpp"
+
+namespace rtype::games::rtype::server {
+
+using shared::ForcePodComponent;
+using shared::ForcePodState;
+using shared::ForcePodTag;
+using shared::NetworkIdComponent;
+using shared::ShootCooldownComponent;
+using shared::TransformComponent;
+
+ForcePodShootingSystem::ForcePodShootingSystem(
+    ProjectileSpawnerSystem* projectileSpawner)
+    : ASystem("ForcePodShootingSystem"),
+      _projectileSpawner(projectileSpawner) {}
+
+void ForcePodShootingSystem::update(ECS::Registry& registry, float deltaTime) {
+    auto podView = registry.view<ForcePodTag, ForcePodComponent,
+                                  TransformComponent, NetworkIdComponent>();
+
+    podView.each([&](ECS::Entity podEntity, const ForcePodTag&,
+                     const ForcePodComponent& forcePod,
+                     const TransformComponent& transform,
+                     const NetworkIdComponent& networkId) {
+        // Only shoot when attached or detached (not when returning)
+        if (forcePod.state == ForcePodState::Returning) {
+            return;
+        }
+
+        // Add or update cooldown component
+        if (!registry.hasComponent<ShootCooldownComponent>(podEntity)) {
+            registry.emplaceComponent<ShootCooldownComponent>(podEntity,
+                                                             SHOOT_COOLDOWN);
+        }
+
+        auto& cooldown = registry.getComponent<ShootCooldownComponent>(podEntity);
+        cooldown.update(deltaTime);
+
+        if (cooldown.canShoot()) {
+            // Reset cooldown
+            cooldown.triggerCooldown();
+
+            // Spawn projectile from Force Pod position
+            if (_projectileSpawner) {
+                _projectileSpawner->spawnPlayerProjectile(
+                    registry, networkId.networkId, transform.x, transform.y);
+            }
+        }
+    });
+}
+
+}  // namespace rtype::games::rtype::server
