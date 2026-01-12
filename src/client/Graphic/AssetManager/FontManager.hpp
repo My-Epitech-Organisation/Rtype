060f322560ff6fadb735cb7f0b1b83880db0eb14
Nduboi <63715705+nduboi@users.noreply.github.com>
2026-01-12 15:16:41 +0100

    feat(FontManager): refactor to inherit from AAssetManager and override load method

--- src/client/Graphic/AssetManager/FontManager.hpp
+++ src/client/Graphic/AssetManager/FontManager.hpp
@@ -14,51 +14,19 @@
 #include <string>
 #include <unordered_map>
 
+#include "AAssetManager.hpp"
 #include "rtype/display/IDisplay.hpp"
 
-class FontManager {
-   private:
-    std::unordered_map<std::string, std::shared_ptr<::rtype::display::IFont>>
-        _assets;
-
+class FontManager : public AAssetManager<::rtype::display::IFont> {
    public:
     FontManager(const FontManager&) = delete;
     FontManager& operator=(const FontManager&) = delete;
 
-    void load(const std::string& id, const std::string& filePath);
-
-    std::shared_ptr<::rtype::display::IFont> get(const std::string& id);
-
-    /**
-     * @brief Check if a font is loaded
-     * @param id The font identifier
-     * @return true if the font is loaded, false otherwise
-     */
-    bool isLoaded(const std::string& id) const;
-
-    /**
-     * @brief Unload a specific font by id
-     * @param id The font identifier to unload
-     * @return true if the font was unloaded, false if not found
-     */
-    bool unload(const std::string& id);
-
-    /**
-     * @brief Unload all fonts
-     */
-    void unloadAll();
-
-    /**
-     * @brief Get the number of loaded fonts
-     * @return Number of fonts currently loaded
-     */
-    std::size_t size() const;
+    void load(const std::string& id, const std::string& filePath) override;
 
     explicit FontManager(::rtype::display::IDisplay* display)
-        : _display(display) {}
+        : AAssetManager<::rtype::display::IFont>(display, "Font") {}
 
-   private:
-    ::rtype::display::IDisplay* _display;
 };
 
 #endif  // SRC_CLIENT_GRAPHIC_ASSETMANAGER_FONTMANAGER_HPP_
