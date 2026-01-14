eb4497f0c91ee2de9b846b74ffff2b6f17f1b289
Samuel Tesson <samuel.tesson@epitech.eu>
2026-01-14 23:25:46 +0100

    fix(EntityType.hpp): standardize formatting of entity type enumeration

--- src/games/rtype/shared/Components/EntityType.hpp
+++ src/games/rtype/shared/Components/EntityType.hpp
@@ -19,16 +19,16 @@ namespace rtype::games::rtype::shared {
  * Values MUST match network::EntityType in Payloads.hpp
  */
 enum class EntityType : uint8_t {
-    Unknown = 0,   // Unknown/uninitialized entity
-    Player = 0,    // Player-controlled ship (same as network::EntityType::Player)
-    Enemy = 1,     // Enemy entity (same as network::EntityType::Bydos)
-    Projectile = 2,// Projectile (same as network::EntityType::Missile)
-    Pickup = 3,    // Collectible item (same as network::EntityType::Pickup)
-    Obstacle = 4,  // Static or moving obstacle
-    ForcePod = 5,  // Force Pod companion entity
-    LaserBeam = 6, // Continuous laser beam weapon
-    Boss = 7,      // Boss entity
-    BossPart = 8   // Boss weak point / body part
+    Unknown = 0,  // Unknown/uninitialized entity
+    Player = 0,  // Player-controlled ship (same as network::EntityType::Player)
+    Enemy = 1,   // Enemy entity (same as network::EntityType::Bydos)
+    Projectile = 2,  // Projectile (same as network::EntityType::Missile)
+    Pickup = 3,      // Collectible item (same as network::EntityType::Pickup)
+    Obstacle = 4,    // Static or moving obstacle
+    ForcePod = 5,    // Force Pod companion entity
+    LaserBeam = 6,   // Continuous laser beam weapon
+    Boss = 7,        // Boss entity
+    BossPart = 8     // Boss weak point / body part
 };
 
 }  // namespace rtype::games::rtype::shared
