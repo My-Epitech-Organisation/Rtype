4d5d6840392f0607fb904dc25315c2d29843f8cd
Nduboi <63715705+nduboi@users.noreply.github.com>
2026-01-15 08:31:31 +0100

    docs(interfaces): add detailed documentation for ILevelMusic and IBackground classes

--- lib/audio/ILevelMusic.hpp
+++ lib/audio/ILevelMusic.hpp
@@ -15,9 +15,31 @@ class AudioLib;
 
 class ILevelMusic {
 public:
+    /**
+     * @brief Loads and plays the music associated with the level.
+     * 
+     * @param audioLib Shared pointer to the AudioLib instance, used to load and control audio playback.
+     */
     virtual void loadLevelMusic(std::shared_ptr<AudioLib> audioLib) = 0;
+
+    /**
+     * @brief Unloads and stops the level music.
+     * 
+     * This function should ensure that the currently playing music is stopped and
+     * any resources specific to this level are released if necessary.
+     */
     virtual void unloadLevelMusic() = 0;
+
+    /**
+     * @brief Retrieves the name of the level music configuration.
+     * 
+     * @return The name of the level music as a string.
+     */
     virtual std::string getLevelMusicName() = 0;
+
+    /**
+     * @brief Virtual destructor.
+     */
     virtual ~ILevelMusic() = default;
 };
 
--- lib/background/IBackground.hpp
+++ lib/background/IBackground.hpp
@@ -12,9 +12,32 @@
 
 class IBackground {
 public:
+    /**
+     * @brief Creates the entities that make up the background.
+     * 
+     * This function is responsible for initializing and spawning all ECS entities
+     * required for the background (e.g., stars, planets, nebulas).
+     */
     virtual void createEntitiesBackground() = 0;
+
+    /**
+     * @brief Unloads and destroys the background entities.
+     * 
+     * This function should remove all entities created by createEntitiesBackground()
+     * from the ECS registry to clean up the level.
+     */
     virtual void unloadEntitiesBackground() = 0;
+
+    /**
+     * @brief Retrieves the name of the background.
+     * 
+     * @return The name of the background as a string.
+     */
     virtual std::string getBackgroundName() = 0;
+
+    /**
+     * @brief Virtual destructor.
+     */
     virtual ~IBackground() = default;
 };
 
